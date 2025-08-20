93bfbb9ddcead18a5a3f167b2d1622f37ed55308
weerasinghaa095-png <weerasinghaa095@gmail.com>
2025-08-20 15:34:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "2IYzXBxC#NWdrg0zcEP2uOTxUEd8mSk6i0-6CHx7uTNlCgHws4es", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
